ec5bd925ad48abc7a5e2c4eaedaab268cc3bbc99
SauceX <mahmoudzak4@gmail.com>
2023-06-18 16:31:23 +0300

    chore: base layout sidebar fix

--- src/components/sidebar/BaseLayout.tsx
+++ src/components/sidebar/BaseLayout.tsx
@@ -39,37 +39,22 @@ const BaseLayout = ({ children, className, pageTitle, description }: Props) => {
         <meta name="description" content={description} />
       </Head>
 
-      {/* <div className="select-none">
+      <div className="select-none">
         <SideNavButton onClick={handleToggle} />
         <SideNav isOpen={isOpen} className="border">
-          <LogoText />
+          {/* LOGO HERE */}
           <SideNavLinks className="flex-grow px-2">
             <SideNavLink href="/home">
               <Home className="me-4 h-5 w-5" /> Home
             </SideNavLink>
-            {session?.user.instructor &&
-              <>
-                <SideNavLink href="/teams">
-                  <Users className="me-4 h-5 w-5" /> Teams
-                </SideNavLink>
-                <SideNavLink href="/forms">
-                  <ClipboardList className="me-4 h-5 w-5" /> Evaluation Forms
-                </SideNavLink>
-              </>
-            }
-            {session?.user.student &&
-              <SideNavLink href="/evaluations">
-                <FileSpreadsheet className="me-4 h-5 w-5" /> Evaluations
-              </SideNavLink>
-            }
-          </SideNavLinks> */}
-      {/* profile picture menu */}
-      {/* <UserProfile mounted={mounted}></UserProfile>
-      </SideNav>
-      {isOpen && (
-        <div onClick={handleToggle} className="fixed inset-0 z-20 bg-black opacity-10 transition-all ease-in-out" aria-hidden="true"></div>
-      )}
-    </div> */}
+            <SideNavLink href="/data">
+              <Users className="me-4 h-5 w-5" /> Data
+            </SideNavLink>
+          </SideNavLinks>
+          {/* profile picture menu */}
+          {/* <UserProfile mounted={mounted}></UserProfile> */}
+        </SideNav>
+      </div>
 
       <main className={cn("md:ms-80 h-screen p-4 pb-6", className)}>
         {children}
@@ -77,15 +62,5 @@ const BaseLayout = ({ children, className, pageTitle, description }: Props) => {
     </>
   );
 };
-
-function LogoText() {
-  return (
-    <div className="text-4xl font-bold  p-4 text-start mt-5 flex ">
-      {/* gradient circle from dark blue to light blue */}
-      <div className="h-[1em] w-[1em] rounded-full borde bg-gradient-to-br from-indigo-500 to-blue-800 mx-2 shadow-lg shadow-cyan-500/60"></div>
-      <div className="bg-clip-text bg-gradient-to-r from-indigo-800 to-blue-600 text-transparent drop-shadow-2xl drop-shadow-indigo-500">TeamRate</div>
-    </div>
-  );
-}
 
 export default BaseLayout;
