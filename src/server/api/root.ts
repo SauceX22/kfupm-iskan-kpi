f6fc9926fbd3f2b70d29cf6048b28bf5b8c0948c
SauceX <mahmoudzak4@gmail.com>
2023-06-18 15:56:48 +0300

    chore: added housing router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { housingRouter } from "~/server/api/routers/housing";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -6,6 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  housing: housingRouter,
 });
 
 // export type definition of API
