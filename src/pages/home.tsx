ebdf7d5bcd5cbddae6342f87ed514149e464c214
SauceX <mahmoudzak4@gmail.com>
2023-06-19 15:00:21 +0300

    chore: home basic table

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,29 @@
-import { Check } from "lucide-react";
 import { type NextPage } from "next";
-import { Button } from "~/components/ui/button/button";
 import BaseLayout from "~/components/sidebar/BaseLayout";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "~/components/ui/card/card";
 import { cn } from "~/lib/utils";
 import { api } from "~/utils/api";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "~/components/ui/table/table";
 
 type Props = {
   // Custom props here
 };
 
-const Home: NextPage = (props: Props) => {
+const Home: NextPage = (props) => {
 
   return (
     <BaseLayout pageTitle="home-page">
@@ -26,7 +39,7 @@ type HousingUnitsTableProps = {
 } & React.ComponentProps<typeof Card>;
 
 const AllHousingUnits = ({ className, ...props }: HousingUnitsTableProps) => {
-  const { data: housingUnitsData, isLoading, error } = api.housing.allHousingUnits.useQuery();
+  const { data: housingUnits, isLoading, error } = api.housing.allHousingUnits.useQuery();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -39,17 +52,44 @@ const AllHousingUnits = ({ className, ...props }: HousingUnitsTableProps) => {
   return (
     <Card className={cn("", className)} {...props}>
       <CardHeader>
-        <CardTitle>Notifications</CardTitle>
-        <CardDescription>You have 3 unread messages.</CardDescription>
+        <CardTitle>All Housing Units</CardTitle>
+        <CardDescription>Housing units data</CardDescription>
       </CardHeader>
       <CardContent className="">
-        Something...
+        <Table>
+          <TableCaption>An overview of housing units.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-center w-[50px]">#</TableHead>
+              <TableHead className="text-center">House S/N</TableHead>
+              <TableHead className="text-center">Court</TableHead>
+              <TableHead className="text-center">BR</TableHead>
+              <TableHead className="text-center">Maintenance Status</TableHead>
+              <TableHead className="text-center">Cleaning Status</TableHead>
+              <TableHead className="text-center">Furnishing Status</TableHead>
+              <TableHead className="text-center">Gardening Status</TableHead>
+              <TableHead className="text-center">Final Check Status</TableHead>
+              <TableHead className="text-center">Submission Status</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {housingUnits.map((house, idx) => (
+              <TableRow key={house.id}>
+                <TableHead className="text-center w-[50px]">{idx + 1}</TableHead>
+                <TableHead className="text-center">{house.unitNumber}</TableHead>
+                <TableHead className="text-center">{house.court}</TableHead>
+                <TableHead className="text-center">{house.bedrooms}</TableHead>
+                <TableHead className="text-center">{house.dateReceivedFromMaintenance?.toLocaleDateString() ?? "Not Yet"}</TableHead>
+                <TableHead className="text-center">{house.dateCompletedCleaning?.toLocaleDateString() ?? "Not Yet"}</TableHead>
+                <TableHead className="text-center">{house.dateCompletedFurnishing?.toLocaleDateString() ?? "Not Yet"}</TableHead>
+                <TableHead className="text-center">{house.dateCompletedGardening?.toLocaleDateString() ?? "Not Yet"}</TableHead>
+                <TableHead className="text-center">{house.dateCheckedAndSubmitedToTenant?.toLocaleDateString() ?? "Not Yet"}</TableHead>
+                <TableHead className="text-center">{house.submissionStatus}</TableHead>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <Check className="mr-2 h-4 w-4" /> Mark all as read
-        </Button>
-      </CardFooter>
     </Card>
   );
 }
