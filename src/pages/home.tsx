78717d1664edd25174976817a910e25ba7e610a2
SauceX <mahmoudzak4@gmail.com>
2023-07-18 10:42:00 +0300

    chore: tables to preview data

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,22 +12,20 @@ import { api } from "~/utils/api";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow
 } from "~/components/ui/table/table";
+import { format } from "date-fns";
+import { HouseSubmissionStatusText } from "~/components/HouseUnitStatus";
 
-type Props = {
-  // Custom props here
-};
 
 const Home: NextPage = (props) => {
 
   return (
     <BaseLayout pageTitle="home-page">
-      <AllHousingUnits />
+      <HousingUnitsLatest />
     </BaseLayout>
   );
 };
@@ -38,8 +36,8 @@ type HousingUnitsTableProps = {
   // Custom props here
 } & React.ComponentProps<typeof Card>;
 
-const AllHousingUnits = ({ className, ...props }: HousingUnitsTableProps) => {
-  const { data: housingUnits, isLoading, error } = api.housing.allHousingUnits.useQuery();
+const HousingUnitsLatest = ({ className, ...props }: HousingUnitsTableProps) => {
+  const { data: housingUnits, isLoading, error } = api.housing.allHouseUnits.useQuery();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -50,43 +48,38 @@ const AllHousingUnits = ({ className, ...props }: HousingUnitsTableProps) => {
   }
 
   return (
-    <Card className={cn("", className)} {...props}>
+    <Card className={cn("w-fit", className)} {...props}>
       <CardHeader>
-        <CardTitle>All Housing Units</CardTitle>
-        <CardDescription>Housing units data</CardDescription>
+        <CardTitle>Latest Housing Units</CardTitle>
+        <CardDescription>Housing units highlights</CardDescription>
       </CardHeader>
-      <CardContent className="">
+      <CardContent>
         <Table>
-          <TableCaption>An overview of housing units.</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="text-center w-[50px]">#</TableHead>
+              <TableHead className="text-center w-[3rem]">#</TableHead>
               <TableHead className="text-center">House S/N</TableHead>
               <TableHead className="text-center">Court</TableHead>
-              <TableHead className="text-center">BR</TableHead>
-              <TableHead className="text-center">Maintenance Status</TableHead>
-              <TableHead className="text-center">Cleaning Status</TableHead>
-              <TableHead className="text-center">Furnishing Status</TableHead>
-              <TableHead className="text-center">Gardening Status</TableHead>
-              <TableHead className="text-center">Final Check Status</TableHead>
+              <TableHead className="text-center">Last Update</TableHead>
               <TableHead className="text-center">Submission Status</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {housingUnits.map((house, idx) => (
-              <TableRow key={house.id}>
-                <TableHead className="text-center w-[50px]">{idx + 1}</TableHead>
-                <TableHead className="text-center">{house.unitNumber}</TableHead>
-                <TableHead className="text-center">{house.court}</TableHead>
-                <TableHead className="text-center">{house.bedrooms}</TableHead>
-                <TableHead className="text-center">{house.dateReceivedFromMaintenance?.toLocaleDateString() ?? "Not Yet"}</TableHead>
-                <TableHead className="text-center">{house.dateCompletedCleaning?.toLocaleDateString() ?? "Not Yet"}</TableHead>
-                <TableHead className="text-center">{house.dateCompletedFurnishing?.toLocaleDateString() ?? "Not Yet"}</TableHead>
-                <TableHead className="text-center">{house.dateCompletedGardening?.toLocaleDateString() ?? "Not Yet"}</TableHead>
-                <TableHead className="text-center">{house.dateCheckedAndSubmitedToTenant?.toLocaleDateString() ?? "Not Yet"}</TableHead>
-                <TableHead className="text-center">{house.submissionStatus}</TableHead>
-              </TableRow>
-            ))}
+            {housingUnits
+              .slice(0, 5)
+              // TODO ensure this shows the last updated house first
+              .sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime())
+              .map((house, idx) => (
+                <TableRow key={house.id}>
+                  <TableCell className="text-center w-[50px]">{idx + 1}</TableCell>
+                  <TableCell className="text-center">{house.unitNumber}</TableCell>
+                  <TableCell className="text-center">{house.court}</TableCell>
+                  <TableCell className="text-center">{format(house.updatedAt, "dd/MM/yy | HH:mm")}</TableCell>
+                  <TableCell className="text-center">
+                    <HouseSubmissionStatusText status={house.submissionStatus} />
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </CardContent>
