034464b19c91aa417d840d9dec392815b886cdcf
SauceX <mahmoudzak4@gmail.com>
2023-07-18 11:04:59 +0300

    refactor: remove redundent eslint disable comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,5 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const cn: (...classes: ClassValue[]) => string = (...classes: ClassValue[]) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
     return twMerge(clsx(...classes))
 }
