b795c4ca128b63d7ffece3703ae2085b05619c10
SauceX <mahmoudzak4@gmail.com>
2023-07-18 10:40:35 +0300

    feat: extra data calculations server helper

---
+++ src/utils/housing-days.ts
@@ -0,0 +1,33 @@
+import {
+    type HouseUnit
+} from "@prisma/client"
+import { differenceInDays } from "date-fns";
+
+const getDayDifference = (date1: Date | null, date2: Date | null) => {
+    if (!date1 || !date2) return null;
+    return differenceInDays(date1, date2);
+}
+
+const processDays = (houses: HouseUnit[]) => {
+    const processingDays = houses.map((house) => {
+        return {
+            ...house,
+            totalDaysToCompleteHousingDuties: getDayDifference(house.dateSubmittedToCleaning, house.dateCompletedGardening),
+            totalDaysToSubmitToCommittee: getDayDifference(house.dateSubmittedToCleaning, house.dateSubmitedToCommittee),
+            totalDaysInMaintenance: getDayDifference(house.dateReceivedFromMaintenance, house.dateReceivedByMaintenance),
+            daysHouseReceivedLaterThanPlanned: getDayDifference(new Date(), house.dateRequiredByPersonnel),
+            totalDaysToPrepareTheHouse: getDayDifference(house.dateReceivedByMaintenance, house.dateSubmitedToCommittee),
+            maintenanceHouseSubmissionStatus: (() => {
+                const dayDiff = getDayDifference(new Date(), house.dateRequiredByPersonnel);
+                if (!house.dateReceivedFromMaintenance || !dayDiff) return "PENDING_MAINTENANCE";
+                else if (dayDiff === 1) return "DONE_ON_TARGET";
+                else if (dayDiff > 1) return "LATE_SUBMISSION";
+                else if (dayDiff < 1) return "EARLY_SUBMISSION";
+                else return "PENDING_MAINTENANCE";
+            })(),
+        }
+    });
+
+    return processingDays;
+}
+
