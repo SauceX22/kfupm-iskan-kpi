af948a28674f6e08a1fe3ace3a2729856b531332
SauceX <mahmoudzak4@gmail.com>
2023-09-05 23:53:03 +0300

    chore: formatting

--- src/components/HouseUnitStatus.tsx
+++ src/components/HouseUnitStatus.tsx
@@ -1,34 +1,35 @@
-import { type HouseSubmissionStatus } from "@prisma/client"
-import { cn } from "~/lib/utils"
+import { type HouseSubmissionStatus } from "@prisma/client";
+import { cn } from "~/lib/utils";
 
 type Props = {
-    status: HouseSubmissionStatus
-}
+  status: HouseSubmissionStatus;
+};
 
 export const HouseSubmissionStatusText = ({ status }: Props) => {
-    const getStatusColor = (status: HouseSubmissionStatus) => {
-        switch (status) {
-            case "NOT_STARTED":
-                return "text-red-600"
-            case "DONE_ON_TARGET" || "EARLY_SUBMISSION":
-                return "text-green-600"
-            case "LATE_SUBMISSION":
-                return "text-yellow-600"
-            default:
-                return "text-orange-500"
-        }
+  const getStatusColor = (status: HouseSubmissionStatus) => {
+    switch (status) {
+      case "NOT_STARTED":
+        return "text-red-600";
+      case "DONE_ON_TARGET" || "EARLY_SUBMISSION":
+        return "text-green-600";
+      case "LATE_SUBMISSION":
+        return "text-yellow-600";
+      default:
+        return "text-orange-500";
     }
+  };
 
-    function toTitleCase(snakeCase: string): string {
-        return snakeCase
-            .split('_')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-            .join(' ');
-    }
+  function toTitleCase(snakeCase: string): string {
+    return snakeCase
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  }
 
-    return (
-        <span className={
-            cn("text-center text-lg font-bold", getStatusColor(status))
-        }>{toTitleCase(status)}</span>
-    )
-}
+  return (
+    <span
+      className={cn("text-center text-lg font-bold", getStatusColor(status))}>
+      {toTitleCase(status)}
+    </span>
+  );
+};
