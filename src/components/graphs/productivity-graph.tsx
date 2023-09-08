e3fc4172bc86f39d98203d9f11d390188493f1c4
SauceX <mahmoudzak4@gmail.com>
2023-09-09 00:12:55 +0300

    chore: create productivity graph component

---
+++ src/components/graphs/productivity-graph.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import MonthIntervalSelector from "~/components/graphs/utils/month-selector";
+import { Button } from "~/components/ui/button";
+import { Card, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import { api } from "~/utils/api";
+import { endOfMonth, format, startOfMonth } from "date-fns";
+import { Loader2, RefreshCwIcon } from "lucide-react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type ProductivityGraphProps = {
+  // ...
+};
+
+const ProductivityGraph = ({}: ProductivityGraphProps) => {
+  // jan 2023 as default start month (because it's the earliest month with the data we have)
+  const [startMonth, setStartMonth] = useState(
+    startOfMonth(new Date("2023-01-01"))
+  );
+  // inclusive of the end month
+  const [endMonth, setEndMonth] = useState(endOfMonth(new Date()));
+
+  const {
+    data: unitStats,
+    refetch,
+    isFetching,
+  } = api.housing.getProductivityStats.useQuery(
+    {
+      startMonth: startMonth,
+      endMonth: endMonth,
+    },
+    {
+      enabled: false,
+      refetchOnMount: false,
+      queryKey: [
+        "housing.getProductivityStats",
+        { startMonth: startMonth, endMonth: endMonth },
+      ],
+    }
+  );
+
+  const handleIntervalChange = (start: Date, end: Date) => {
+    setStartMonth(start);
+    setEndMonth(end);
+    console.log(format(start, "MMM-yyyy"), format(end, "MMM-yyyy"));
+  };
+
+  return (
+    <Card className="w-fit p-4">
+      <CardHeader>
+        <CardTitle>Productivity Change Graph</CardTitle>
+      </CardHeader>
+      <LineChart
+        width={800}
+        height={500}
+        data={unitStats?.stats.map((stat) => ({
+          month: stat.month,
+          units: stat.monthStats.unitsCompleted.length,
+        }))}>
+        <XAxis dataKey="month" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="units"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+
+      <CardFooter className="w-full flex flex-col gap-2">
+        {/* This component deals with (start as startOfMonth) and (end as endOfMonth) to ensure full interval */}
+        <MonthIntervalSelector
+          startMonth={startOfMonth(new Date("2023-01-01"))}
+          endMonth={endOfMonth(new Date())}
+          onValueChange={async (startMonth, endMonth) =>
+            // eslint-disable-next-line @typescript-eslint/await-thenable
+            await handleIntervalChange(startMonth, endMonth)
+          }
+        />
+        <Button
+          className="w-1/2 self-center"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onClick={async () =>
+            await refetch({
+              queryKey: [
+                "housing.getProductivityStats",
+                { startMonth: startMonth, endMonth: endMonth },
+              ],
+            })
+          }
+          disabled={isFetching}>
+          {isFetching ? (
+            <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+          ) : (
+            <RefreshCwIcon className="w-4 h-4 mr-2" />
+          )}
+          Refresh
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProductivityGraph;
