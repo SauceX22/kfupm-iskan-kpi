42092e0710c5ed0dd2ae40d286a82d00dbce19b1
SauceX <mahmoudzak4@gmail.com>
2023-09-08 18:37:12 +0300

    fix: default to 6 months backwards

--- src/utils/housing-kpi.ts
+++ src/utils/housing-kpi.ts
@@ -16,7 +16,7 @@ const getDayDifference = (date1: Date | null, date2: Date | null) => {
 };
 
 const lastXMonths = (monthsBack: number) =>
-  Array.from({ length: (monthsBack = 7) }, (_, i) => {
+  Array.from({ length: (monthsBack = 6) }, (_, i) => {
     const date = subMonths(startOfMonth(new Date()), i);
     // if the date is before "Jan-23", return nothing and
     // filter out any empty values at the end
